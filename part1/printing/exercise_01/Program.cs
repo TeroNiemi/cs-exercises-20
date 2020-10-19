45aa38543ee160a0bc2bca9e10f132221a404c1e
TeroNiemi <70256874+TeroNiemi@users.noreply.github.com>
2020-10-19 11:51:03 +0300

    Update Program.cs
    19.10.2020

--- part1/printing/exercise_01/Program.cs
+++ part1/printing/exercise_01/Program.cs
@@ -6,7 +6,7 @@ namespace exercise_01
     {
         public static void Main(string[] args)
         {
-            // Add your code here:
+            // Add your code here
             
         }
     }
